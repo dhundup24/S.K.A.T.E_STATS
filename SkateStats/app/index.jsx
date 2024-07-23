3260da5a5db0d5fcc1b51d609d21a407f301e171
dhundup24 <53953323+dhundup24@users.noreply.github.com>
2024-07-22 23:39:42 -0600

    got the onboarding page elements on screen as a very rough version

--- SkateStats/app/index.jsx
+++ SkateStats/app/index.jsx
@@ -1,13 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { ScrollView, Text, View, Image } from 'react-native';
 import { Link} from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../constants';
+import CustomButton from '../components/CustomButton';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Skate stats logo here!</Text>
-      <StatusBar style="auto"/>
-      <Link href="/home" styler={{color:'blue'}}>Go to Home</Link>
-    </View>
+    //SafeAreaView ensures that the content in this component doesn't overlap with other stuff like bottom tabs
+    <SafeAreaView className="bg-primary h-full">
+        <ScrollView contentContainerStyle={{height: '100%'}}>
+            <View className="w-full justify-center 
+            items-center h-full px-4">
+                <Image 
+                    source={images.skateStatsLogo}
+                    className="w-[400px] h-[200px]"
+                    resizeMode="contain"
+                />
+
+                <View className="relative mt-5">
+                    <Text className="text-3xl text-white font-bold text-center">Climb the ranks and be the best in{' '}
+                    <Text className= "text-secondary-200">S.K.A.T.E STATS</Text>
+                    </Text>
+
+                    <CustomButton
+                        title="Continue with Email"
+                        handlePress={() => {}}
+                        containerStyles="w-full mt-7"
+                    />
+                </View>
+            </View>
+        </ScrollView>
+    </SafeAreaView>
   );
 }
