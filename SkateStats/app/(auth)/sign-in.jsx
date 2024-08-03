2820e6c1bccff2a77652520eccfd2415d1d9d89b
dhundup24 <53953323+dhundup24@users.noreply.github.com>
2024-08-03 17:56:06 -0600

    current progress2

--- SkateStats/app/(auth)/sign-in.jsx
+++ SkateStats/app/(auth)/sign-in.jsx
@@ -1,15 +1,30 @@
 import { View, Text, ScrollView, Image } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import {images} from '../../constants'
+import FormField from '../../components/FormField'
 
 const SignIn = () => {
+  const [form, setForm] = useState( {email:'', password: ''})
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
         <View className="w-full justify-center h-full px-4 my-6">
-          <Image source= {images.skateStatsLogo} resizeMode='contain' className="w-[200px] h-[205px]"/>
-          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold"> Log into S.K.A.T.E STATS here!</Text>
+          <Image source= {images.logo} resizeMode='contain' className="w-[115px] h-[35px]"/>
+          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold"> Log into Auora</Text>
+        <FormField
+          title="Email"
+          value={form.email}
+          handleChangeText={(e) => setForm({ ...form, email: e})}
+          otherStyles="mt-7"
+          keyboardType="email-address"/>
+
+        <FormField
+          title="Password"
+          value={form.password}
+          handleChangeText={(e) => setForm({ ...form, password: e})}
+          otherStyles="mt-7"
+        />
         </View>
       </ScrollView>
     </SafeAreaView>
--- SkateStats/components/FormField.jsx
+++ SkateStats/components/FormField.jsx
@@ -17,12 +17,12 @@ const FormField = ({
     <View className={`space-y-2 ${otherStyles}`}>
       <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
 
-      <View className="w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary flex flex-row items-center">
+      <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center flex-row">
         <TextInput
           className="flex-1 text-white font-psemibold text-base"
           value={value}
           placeholder={placeholder}
-          placeholderTextColor="#7B7B8B"
+          placeholderTextColor="#7b7b8b"
           onChangeText={handleChangeText}
           secureTextEntry={title === "Password" && !showPassword}
           {...props}
